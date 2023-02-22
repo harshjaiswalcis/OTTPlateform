60e0a943faa703a46c6294f1ae62530ea7e06314
harsh.j@cisinlabs.com <harsh.j@cisinlabs.com>
2023-02-22 16:07:02 +0530

    appbar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -23,52 +23,119 @@ class _HomeScreenState extends State<HomeScreen> {
       body: ValueListenableBuilder(
         valueListenable: appProvider.currentTab,
         builder: (context, value, child) {
-          return Row(
+          return Column(
             children: [
-              Expanded(
-                  flex: 1,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      ...List.generate(appProvider.tabs.length, (index) {
-                        return ListTile(
-                          leading: const Icon(Icons.movie_creation_outlined),
-                          title: Text(appProvider.tabs[index]),
-                          selected: appProvider.tabs[index] == value,
-                          selectedTileColor: Colors.blueGrey,
-                          selectedColor: Colors.white,
-                          onTap: () {
-                            appProvider.currentTab.value =
-                                appProvider.tabs[index];
-                          },
-                        );
-                      })
-                    ],
-                  )),
-              Expanded(
-                flex: 7,
-                child: value == "movie"
-                    ? const Movie()
-                    : value == "series"
-                        ? const Series()
-                        : value == "screen4"
-                            ? const Screen4()
-                            : value == "screen5"
-                                ? const Screen5()
-                                : value == "home"
-                                    ? Container(
-                                        color: Colors.orangeAccent,
-                                        child: Column(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.center,
-                                          children: <Widget>[
-                                            Text(
-                                              value,
+              Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      AppIcons.ott,
+                      height: 70,
+                      width: 250,
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 0.2.getScreenWidth,
+                          child: const TextField(
+                            decoration: InputDecoration(
+                              // isDense: true,
+                              hintText: "Search",
+                              contentPadding: EdgeInsets.all(8),
+                              suffix: Icon(AppIcons.mic),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 8,
+                        ),
+                        DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: AppColors.mediumGray,
+                            border: Border.all(
+                              width: 1.0,
+                              color: AppColors.darkGray,
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(3)),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(5.0),
+                            child: Icon(AppIcons.notification),
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        DecoratedBox(
+                          decoration: BoxDecoration(
+                            color: AppColors.mediumGray,
+                            border: Border.all(
+                              width: 1.0,
+                              color: AppColors.darkGray,
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(3)),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.all(5.0),
+                            child: Icon(AppIcons.person),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                      flex: 1,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ...List.generate(appProvider.tabs.length, (index) {
+                            return ListTile(
+                              leading:
+                                  const Icon(Icons.movie_creation_outlined),
+                              title: Text(appProvider.tabs[index]),
+                              selected: appProvider.tabs[index] == value,
+                              selectedTileColor: Colors.blueGrey,
+                              selectedColor: Colors.white,
+                              onTap: () {
+                                appProvider.currentTab.value =
+                                    appProvider.tabs[index];
+                              },
+                            );
+                          })
+                        ],
+                      )),
+                  Expanded(
+                    flex: 7,
+                    child: value == "movie"
+                        ? const Movie()
+                        : value == "series"
+                            ? const Series()
+                            : value == "screen4"
+                                ? const Screen4()
+                                : value == "screen5"
+                                    ? const Screen5()
+                                    : value == "home"
+                                        ? Container(
+                                            color: Colors.orangeAccent,
+                                            child: Column(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: <Widget>[
+                                                Text(
+                                                  value,
+                                                ),
+                                              ],
                                             ),
-                                          ],
-                                        ),
-                                      )
-                                    : const SizedBox.shrink(),
+                                          )
+                                        : const SizedBox.shrink(),
+                  ),
+                ],
               ),
             ],
           );
--- lib/utils/constants/app_icons.dart
+++ lib/utils/constants/app_icons.dart
@@ -2,5 +2,10 @@ import 'package:ott_demo/utils/exports/common_exports.dart';
 
 class AppIcons {
   static const String add = "assets/icons/add.svg";
+  static const String ott = "assets/ott.png";
   static const IconData cross = Icons.close;
+  static const IconData mic = Icons.mic;
+
+  static const IconData person = Icons.person_outline;
+  static const IconData notification = Icons.notifications_outlined;
 }
